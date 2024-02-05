67627e489ad408b2b517693c05a23b99c0262be5
claudiahW <murungiclaud@gmail.com>
2024-02-05 20:37:20 +0300

    use if else statements to show days of the week

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
-if(5 > 7) {
-    console.log("5 > 7")
-}else if(7 > 8){
-    console.log("7 > 8")
-}else {
-    console.log("Nothing")
+var currentDay = "Mon";
+if(currentDay === "Mon") {
+    console.log('Timings: 10:00-06:00')
+}else if (currentDay === "Tue") {
+    console.log('Timings: 9:00-05:00') 
+}else if (currentDay === "Wed") {
+    console.log('Timings: 9:30-05:30') 
+}else if (currentDay === "Thu") {
+    console.log('Timings: 10:15-6:15') 
+}else if (currentDay === "Fri") {
+    console.log('Timings: 9:05-05:05') 
+}else if (currentDay === "Sat") {
+    console.log('Timings: 9:00-05:00') 
+}else if (currentDay === "Sun") {
+    console.log('Timings: 9:00-01:00') 
 };
+
