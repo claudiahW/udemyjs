e515e1c9b45b65ea7ec26de71d264e9fbb6b2674
claudiahW <murungiclaud@gmail.com>
2024-02-05 15:59:50 +0300

    use quotes in strings

--- scripts.js
+++ scripts.js
@@ -1,16 +1,9 @@
+var myFirstString = "I love JavaScript";
 
+console.log(typeof(myFirstString));
 
-var num1 = 10; //integer
-var num2 = 15.7; //Decimal number
+var mySecondString = 'We will learn JavaScript together';
+console.log(typeof(mySecondString));
 
-
-
-var num3 = 25;
-var sum = num3 + num1;
-console.log(num1.toString());
-
-var strNum1 = "10";
-var strNum2 = "11.5";
-
-console.log(parseInt(strNum1));
-console.log(parseInt(strNum2));
+var doubleQuoteString = "This is a \"JavaScript\" code";
+console.log(doubleQuoteString);
