bb90259205ea1b3cdf8b26195f322c7eff9bded6
claudiahW <murungiclaud@gmail.com>
2024-02-05 15:35:58 +0300

    usew inbuilt number functions

--- scripts.js
+++ scripts.js
@@ -7,4 +7,10 @@ var num2 = 15.7; //Decimal number
 
 var num3 = 25;
 var sum = num3 + num1;
-console.log(sum);
+console.log(num1.toString());
+
+var strNum1 = "10";
+var strNum2 = "11.5";
+
+console.log(parseInt(strNum1));
+console.log(parseInt(strNum2));
