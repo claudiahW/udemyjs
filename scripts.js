1432f431c777530e8a1909cecc17858625676146
claudiahW <murungiclaud@gmail.com>
2024-02-06 23:39:21 +0300

    demonstrate use of arithmetic operators, assignment opersatiors

--- scripts.js
+++ scripts.js
@@ -1,45 +1,10 @@
-var currentDay = "Thu";
+var num1 = 15;
+var num2 = 30;
 
-if(currentDay === "Mon") {
-    console.log('Timings: 10:00-06:00')
-}else if (currentDay === "Tue") {
-    console.log('Timings: 9:00-05:00') 
-}else if (currentDay === "Wed") {
-    console.log('Timings: 9:30-05:30') 
-}else if (currentDay === "Thu") {
-    console.log('Timings: 10:15-6:15') 
-}else if (currentDay === "Fri") {
-    console.log('Timings: 9:05-05:05') 
-}else if (currentDay === "Sat") {
-    console.log('Timings: 9:00-05:00') 
-}else if (currentDay === "Sun") {
-    console.log('Timings: 9:00-01:00') 
-};
+ //  =
+// we assigned the value 0f 15 to our variable num1
 
-switch(currentDay) {
-    case "Mon":
-        console.log("Timings: 10:00-06:00")
-        break;
-    case "Tue":
-        console.log("Timings: 09:00-05:00")
-        break;    
-    case "Wed":
-        console.log("Timings: 09:30-05:30")
-        break;
-    case "Thu":
-        console.log("Timings: 10:15-6:15")
-        break;
-    case "Fri":
-        console.log("Timings: 9:05-05:05")
-        break;
-    case "Sat":
-        console.log("Timings: 9:00-05:00")
-        break;
-    case "Sun":
-        console.log("Timings: 9:00-01:00")
-        break;  
-    default:
-        console.log("Timings: 9:00-01:00")
-
-}
+// +=
+num1 %= 10;
+console.log(num1);
 
