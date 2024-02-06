81608f3642fc9ac6c4732ceb0c66a04bf2b08e7a
claudiahW <murungiclaud@gmail.com>
2024-02-06 22:35:25 +0300

    demonstrate use of switch statements in javascript

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-var currentDay = "Mon";
+var currentDay = "Thu";
+
 if(currentDay === "Mon") {
     console.log('Timings: 10:00-06:00')
 }else if (currentDay === "Tue") {
@@ -14,4 +15,31 @@ if(currentDay === "Mon") {
 }else if (currentDay === "Sun") {
     console.log('Timings: 9:00-01:00') 
 };
+
+switch(currentDay) {
+    case "Mon":
+        console.log("Timings: 10:00-06:00")
+        break;
+    case "Tue":
+        console.log("Timings: 09:00-05:00")
+        break;    
+    case "Wed":
+        console.log("Timings: 09:30-05:30")
+        break;
+    case "Thu":
+        console.log("Timings: 10:15-6:15")
+        break;
+    case "Fri":
+        console.log("Timings: 9:05-05:05")
+        break;
+    case "Sat":
+        console.log("Timings: 9:00-05:00")
+        break;
+    case "Sun":
+        console.log("Timings: 9:00-01:00")
+        break;  
+    default:
+        console.log("Timings: 9:00-01:00")
+
+}
 
