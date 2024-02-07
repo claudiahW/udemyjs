3a37eef192c5c53590d732fe1ee9856855c7ea8a
claudiahW <murungiclaud@gmail.com>
2024-02-07 19:01:54 +0300

    illustrate use of for loops

--- scripts.js
+++ scripts.js
@@ -1,2 +1,5 @@
-var mFriends = ["Mary", "Martha", "James", "Lilian"];
-console.log(mFriends[2]);
+ var mList = [1, 2, 3, 4, 5]
+ var pos;
+ for(pos = 0; pos < mList.length; pos++) {
+    console.log("Position => " + "Value => " + mList[pos])
+ };
