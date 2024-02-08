675d748d75aaf1dce19c112ea8152559bd7d2cc3
claudiahW <murungiclaud@gmail.com>
2024-02-08 18:33:47 +0300

    add a function that will allow our counter to stop once it gets to zero

--- scripts.js
+++ scripts.js
@@ -1,2 +1,14 @@
-console.log("JS loaded");
 
+
+var countDownElement =
+ document.getElementById
+ ("countdown");
+
+var initialCountDownVal = 
+countDownElement.innerHTML;
+
+setInterval(function(){
+   initialCountDownVal = initialCountDownVal > 0 ? initialCountDownVal -1 : 0 ;
+   
+   countDownElement.innerHTML =  initialCountDownVal;
+}, 1000);
