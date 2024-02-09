a7b00aa437b757e70d520fcd98f00509c1ff5fc9
claudiahW <murungiclaud@gmail.com>
2024-02-09 14:46:47 +0300

    demonstrate use of onclick button to show an alert message once the button is clicked

--- scripts.js
+++ scripts.js
@@ -1,17 +1,7 @@
-var countDownElement = document.getElementById("countdown");
-var bgImage = document.getElementById("bg-img");
+var btn = document.getElementById("btn-click");
 
-var initialCountdownVal = countDownElement.innerHTML;
-
-var interval = setInterval(function(){
-   initialCountdownVal = initialCountdownVal > 0 ? initialCountdownVal -1 : 0;
-    countDownElement.innerHTML = initialCountdownVal - 1;
-    countDownElement.style.fontSize = initialCountdownVal * 100 + "px"
-
-bgImage.style.width = initialCountdownVal * 10 + "%"
-
-if(initialCountdownVal <= 0){
-   clearInterval(interval);
+function onBtnClick() {
+   alert("Button Clicked");
 }
 
-}, 1000);
+btn.onclick = onBtnClick();
